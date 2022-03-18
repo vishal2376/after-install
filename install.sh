a28110ebe5aa612a4ac7fb32cb3394865245e1aa
vishal2376 <vishalsingh2376@gmail.com>
2022-03-18 17:39:13 +0530

    todo added

--- install.sh
+++ install.sh
@@ -128,3 +128,4 @@ sudo apt-get autoremove
 echo "\n\n----------[TODO] : Copy wallpapers to '.local\share\wallheaven'----------\n"
 echo "\n----------[TODO] : Install vim plugins [:PlugInstall]----------\n"
 echo "\n----------[TODO] : Configure github ssh keys----------\n"
+echo "\n----------[TODO] : Configure Brave browser Sync----------\n"
