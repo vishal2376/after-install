72037015016b23d37b8f881970ac5525b4361025
vishal2376 <vishalsingh2376@gmail.com>
2022-06-07 11:53:42 +0530

    fix: dir already exist

--- post_install.sh
+++ post_install.sh
@@ -5,6 +5,10 @@ comfortable-swipe autostart on
 sudo comfortable-swipe start
 
 
+echo "\n---------Removing Directories--------------\n"
+rm -r ~/Documents
+rm -r ~/Videos
+
 echo "\n---------Creating Symbolic links--------------\n"
 ln -s /mnt/ECHO/Documents ~
 ln -s /mnt/ECHO/Videos ~
