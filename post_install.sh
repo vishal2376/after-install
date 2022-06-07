1662c97528da258930dce84505d68f324c6ddf7b
vishal2376 <vishalsingh2376@gmail.com>
2022-06-07 11:51:25 +0530

    cmd:add symbolic links

--- post_install.sh
+++ post_install.sh
@@ -3,3 +3,9 @@ echo "\n---------Activating Touch Guestures------------\n"
 comfortable-swipe start
 comfortable-swipe autostart on
 sudo comfortable-swipe start
+
+
+echo "\n---------Creating Symbolic links--------------\n"
+ln -s /mnt/ECHO/Documents ~
+ln -s /mnt/ECHO/Videos ~
+ln -s /mnt/ECHO/Documents/AndroidStudioProjects ~
