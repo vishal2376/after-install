9eb643fd290df6d25446d5f84fbb3bb18e61ef6f
vishal2376 <vishalsingh2376@gmail.com>
2022-06-07 13:08:30 +0530

    minor changes

--- install.sh
+++ install.sh
@@ -23,12 +23,14 @@ echo "\n----------Adding GPG Keys----------\n"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg https://brave-browser-apt-nightly.s3.brave.com/brave-browser-nightly-archive-keyring.gpg
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
 sudo add-apt-repository ppa:o2sh/onefetch
 
 echo "\n----------Adding Channel----------\n"
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg arch=amd64] https://brave-browser-apt-nightly.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-nightly.list
 echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 
 echo "\n----------Updating the sources----------\n"
 sudo apt-get update
@@ -37,6 +39,7 @@ echo "\n----------Installing app from sources----------\n"
 sudo apt-get install sublime-text -y
 sudo apt-get install brave-browser-nightly -y
 sudo apt install mono-devel -y
+sudo apt-get install unityhub -y
 sudo apt-get install onefetch -y
 
 #############################
@@ -137,6 +140,7 @@ cp -vr .vim ~
 ################################
 echo "\n\n----------Installing scripts dependencies----------\n"
 sudo apt-get install jq pup dmenu xclip -y
+pip install yt-dlp
 
 
 echo "\n----------Color Scheme changer----------\n"
@@ -157,8 +161,6 @@ sudo chmod -R +x ~/.config/sxiv/
 ########################
 # Optimzing & Cleaning #
 ########################
-echo "\n\n----------Installing Auto CPU Freq----------\n"
-sudo snap install auto-cpufreq  
 
 echo "\n\n----------Removing unwanted tools----------\n"
 sudo apt-get autoremove -y
@@ -171,6 +173,7 @@ sudo apt-get autoremove -y
 echo "\n\n----------[TODO] : Copy wallpapers to '.local\share\wallheaven'----------"
 echo "\n----------[TODO] : Change Drive Name(ECHO) & then run PostInstall Script----------"
 echo "\n----------[TODO] : Install vim plugins [:PlugInstall]----------"
+echo "\n----------[TODO] : Install Autofreq from github----------"
 echo "\n----------[TODO] : Configure github ssh keys----------"
 echo "\n----------[TODO] : Configure Brave browser Sync----------"
 echo "\n----------[TODO] : Set themes, fonts , icons ,etc. using Tweak tool----------"
