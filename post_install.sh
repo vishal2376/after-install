3f89a4231ea5a5be6794d7425b23628bb2ab94c9
vishal2376 <vishalsingh2376@gmail.com>
2022-10-21 23:30:01 +0530

    upgrage to ubuntu 2022

--- post_install.sh
+++ post_install.sh
@@ -7,9 +7,15 @@ sudo comfortable-swipe start
 
 echo "\n---------Removing Directories--------------\n"
 rm -r ~/Documents
+rm -r ~/Pictures
+rm -r ~/Downloads
+rm -r ~/Music
 rm -r ~/Videos
 
 echo "\n---------Creating Symbolic links--------------\n"
 ln -s /mnt/ECHO/Documents ~
+ln -s /mnt/ECHO/Pictures ~
+ln -s /mnt/ECHO/Downloads ~
+ln -s /mnt/ECHO/Music ~
 ln -s /mnt/ECHO/Videos ~
-ln -s /mnt/ECHO/Documents/AndroidStudioProjects ~
+ln -s /mnt/ECHO/Unity ~
