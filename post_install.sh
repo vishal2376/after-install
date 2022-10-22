5732ecc4e4326a13bf42b1157601dd75fb8ec225
vishal2376 <vishalsingh2376@gmail.com>
2022-10-22 11:31:20 +0530

    cmd added:set custom shortcuts

--- post_install.sh
+++ post_install.sh
@@ -19,3 +19,17 @@ ln -s /mnt/ECHO/Downloads ~
 ln -s /mnt/ECHO/Music ~
 ln -s /mnt/ECHO/Videos ~
 ln -s /mnt/ECHO/Unity ~
+
+echo "\n---------Adding custom shortcuts--------------\n"
+./scripts/set_shortcut.py 'Terminal' 'gnome-terminal' '<Super>Return'
+./scripts/set_shortcut.py 'Random Wallpaper' 'change-wallpaper' '<Super><Shift>t'
+./scripts/set_shortcut.py 'Download Wallpaper' 'waldl' '<Super><Shift>w'
+./scripts/set_shortcut.py 'Sublime Text 4' 'subl' '<Super><Shift>s'
+./scripts/set_shortcut.py 'Shutdown' 'gnome-session-quit --power-off' '<Super>F4'
+./scripts/set_shortcut.py 'Blender' 'blender' '<Super><Shift>b'
+./scripts/set_shortcut.py 'Google' 'google' '<Super><Shift>g'
+./scripts/set_shortcut.py 'Discord' 'discord' '<Super><Shift>d'
+./scripts/set_shortcut.py 'Kill Application' 'xkill' '<Super>Escape'
+./scripts/set_shortcut.py 'Telegram' 'telegram-desktop' '<Super><Shift>c'
+./scripts/set_shortcut.py 'Enable Mouse Scroll' 'mouse-scroll 3' '<Super><Shift>F12'
+./scripts/set_shortcut.py 'Disable Mouse Scroll' 'gnome-terminal -- pkill imwheel' '<Super>F12'
