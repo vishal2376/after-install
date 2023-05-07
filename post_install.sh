f3a3e6d963620126908bb27b2d9c1677cd7c53aa
vishal2376 <vishalsingh2376@gmail.com>
2023-05-07 13:40:11 +0530

    update: rm tlp

--- post_install.sh
+++ post_install.sh
@@ -8,14 +8,12 @@ echo "\n---------Removing Directories--------------\n"
 rm -r ~/Documents
 rm -r ~/Pictures
 rm -r ~/Downloads
-rm -r ~/Music
 rm -r ~/Videos
 
 echo "\n---------Creating Symbolic links--------------\n"
 ln -s /mnt/ECHO/Documents ~
 ln -s /mnt/ECHO/Pictures ~
 ln -s /mnt/ECHO/Downloads ~
-ln -s /mnt/ECHO/Music ~
 ln -s /mnt/ECHO/Videos ~
 ln -s /mnt/ECHO/Unity ~
 cp -r /mnt/ECHO/wallhaven ~/.local/share
@@ -40,10 +38,6 @@ echo "\n---------Adding custom shortcuts--------------\n"
 ./scripts/set_shortcut.py 'Telegram' 'telegram-desktop' '<Super><Shift>c'
 ./scripts/set_shortcut.py 'Enable Mouse Scroll' 'mouse-scroll 3' '<Super><Shift>F12'
 ./scripts/set_shortcut.py 'Disable Mouse Scroll' 'gnome-terminal -- pkill imwheel' '<Super>F12'
-
-echo "\n---------Activating tlp--------------\n"
-sudo systemctl enable tlp.service
-sudo tlp start
 
 echo "\n---------Activating preload--------------\n"
 sudo preload
