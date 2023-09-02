e24b00cef3771e118ad1f53739e371392086197d
Vishal Singh <vishalsingh2376@gmail.com>
2023-09-02 10:44:19 +0530

    add cmd: install nvim

--- install.sh
+++ install.sh
@@ -95,6 +95,18 @@ flatpak install flathub io.neovim.nvim
 
 
 #####################
+# Installing Neovim #
+#####################
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+nvim
+
+#####################
 # Installing Swipes #
 #####################
 
