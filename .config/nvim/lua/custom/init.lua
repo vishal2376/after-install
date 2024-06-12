cbefd53c1a22ab1da5ea9e42db06234c998ad3dd
vishal2376 <vishalsingh2376@gmail.com>
2024-06-12 16:04:34 +0530

    chore[nvim]: enabled transparency

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -13,7 +13,7 @@ opt.guifont = "JetBrainsMono Nerd Font:h15"
 
 -- Neovide Settings
 if vim.g.neovide then
-    -- vim.g.neovide_transparency = 0.9
+    vim.g.neovide_transparency = 0.9
     vim.g.neovide_cursor_vfx_mode = "railgun"
     vim.g.neovide_hide_mouse_when_typing = true
     vim.g.neovide_remember_window_size = true
